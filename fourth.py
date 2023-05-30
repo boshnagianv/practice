a9288e7a8d441f12b846afb45b80289132255dba
boshnagianv <boshnagianv@gmail.com>
2023-05-30 17:41:06 +0400

    lil commit

--- fourth.py
+++ fourth.py
@@ -27,3 +27,84 @@ output: Hello This Is Pythonlobby
 
 
 '''
+
+'''
+1)Write a Python program to get a string from a given string where all occurrences of its "r" char have been changed to '$'.
+example: 'restart'
+Result : '$esta$t'
+'''
+
+def repl(word):
+    return word.replace("r", "$")
+
+ourWord = input("Enter your word: ")
+print(repl(ourWord))
+
+'''
+2)Write a Python program to swap comma and dot in a string.
+Sample string: "32.054,23" REVIEWwwwwwwwwwwwwwwww
+Expected Output: "32,054.23"
+'''
+
+string = input("Enter the float number: ")
+string_list = list(string)
+
+for char in range(len(string_list)):
+    if string_list[char] == ".":
+        string_list[char] = ","
+    elif string_list[char] == ",":
+        string_list[char] = "."
+
+swapped_string = "".join(string_list)
+print(swapped_string)
+
+#3) Write a Python program to compute sum of digits of a given string.
+
+number = input("Enter your number: ")
+sum = 0
+
+for v in number:
+    sum += int(v)
+print(sum)
+
+
+
+#4)Write a Python program to remove spaces from a inputed sentence.
+
+'''
+
+def noSpace(sentence):
+    for char in sentence:
+        if char == " ":
+            sentence[char].replace(" ", "g")
+    return sentence
+
+phrase = input("Enter your sentence: ")
+
+print(noSpace(phrase))
+
+'''
+
+def noSpace(sentence):
+    modified_sentence = ""
+    for char in sentence:
+        if char == " ":
+            modified_sentence += ""
+        else:
+            modified_sentence += char
+    return modified_sentence
+
+phrase = input("Enter your sentence: ")
+
+print(noSpace(phrase)) #REVIEW
+
+
+
+#5)Write python program to print even length words from inputed sentence.
+
+sent = input("Enter your evenly sentence: ")
+
+
+
+
+
